f0ebe83b80c0f63e5fa49380f8b77e2b8ef10a37
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-04-27 22:55:32 +0530

    Binary Search

--- chapter-14/SortedAndRotatedArray.java
+++ chapter-14/SortedAndRotatedArray.java
@@ -1,3 +1,36 @@
 public class SortedAndRotatedArray {
-    
+    public static int bSearch (int nums[] , int key){
+        int low =0;
+        int high = nums.length-1;
+
+        while (low <= high){
+            int mid = (low+high)/2;
+
+            if (nums[mid]==key){
+                return mid;
+            }
+            if (nums[low]<nums[mid]){
+                if (key>=nums[low] && key < nums[mid]){
+                    high = mid-1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (key>nums[mid] && key<nums[high]){
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
+            }
+        } return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {1,2,4,5,7,10,3};
+        int index = bSearch(nums, 4);
+        System.out.println("Index is "+index);
+    }
 }
