cc8c0375b3ca35fe6fc95763306cfd5d8e2c1418
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-04-28 21:55:24 +0530

    Bubble-Sort

---
+++ chapter-15/BubbleSort.java
@@ -0,0 +1,30 @@
+
+public class BubbleSort {
+    //Bubble Sort for Descending Order
+    public static void BubbleSort(int arr[]) {
+        for (int turn = 0; turn < arr.length-1; turn++) {
+            for (int j = 0; j < arr.length-1-turn; j++) {
+                //bubble sort for decending order change greater than sign to change to ascending order
+                if (arr[j] < arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void PrintArray (int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1,8, 7, 4, 5, 3,1 };
+        BubbleSort(arr);
+        PrintArray(arr);
+    }
+}
