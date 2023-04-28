febdcf5e28b38f5be3e5ae9feaacb9b1ed0d491e
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-04-28 22:51:56 +0530

    Selection Sort

---
+++ chapter-15/SelectionSort.java
@@ -0,0 +1,31 @@
+
+public class SelectionSort {
+    //Selection Sort for Descending Order
+    public static void SelectionSort (int arr[]){
+        for(int i=0; i<arr.length-1;i++){
+            int minPos =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPos]<arr[j]){
+                    minPos=j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    
+    public static void PrintArray (int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1,8, 7, 4, 5, 3,1 };
+        SelectionSort(arr);
+        PrintArray(arr);
+    }
+}
