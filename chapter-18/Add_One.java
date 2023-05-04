ca35ebece332462999f54171587525b24a3564bc
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-04 16:07:29 +0530

    add 1 to number

---
+++ chapter-18/Add_One.java
@@ -0,0 +1,10 @@
+public class Add_One {
+    public static void main(String[] args) {
+        int i = 6;
+        System.out.println(i + " + " + 1 + " is " + -~i);
+         i = -4;
+        System.out.println(i + " + " + 1 + " is " + -~i);
+         i = 0;
+        System.out.println(i + " + " + 1 + " is " + -~i);
+    }
+}
