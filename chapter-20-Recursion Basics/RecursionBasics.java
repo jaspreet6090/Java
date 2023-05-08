815bd9b1431d15d029462dd5f758de0e9db90813
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-09 00:36:03 +0530

    basucs

--- chapter-20-Recursion Basics/RecursionBasics.java
+++ chapter-20-Recursion Basics/RecursionBasics.java
@@ -1,3 +1,22 @@
 public class RecursionBasics {
-    
+    public static void printDec(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+
+    public static void printInc(int n){
+        if(n==0){
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+         printDec(5); 
+         System.out.println();
+         printInc(5);
+    }
 }
