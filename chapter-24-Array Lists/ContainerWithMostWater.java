c61a100925161bbe740df04d4f9fe72501d00c4a
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-27 19:42:08 +0530

    2 - Pointer approach -> O(n)

--- chapter-24-Array Lists/ContainerWithMostWater.java
+++ chapter-24-Array Lists/ContainerWithMostWater.java
@@ -1,14 +1,38 @@
 import java.util.*;
+
 public class ContainerWithMostWater {
-    public static int storeWater(ArrayList<Integer> height){
-        //BruteForce -> O(n^2)
+    public static int storeWater(ArrayList<Integer> height) {
+        // BruteForce -> O(n^2)
+        // int maxWater = 0;
+        // for(int i = 0; i < height.size(); i++) {
+        // for(int j = i + 1; j < height.size(); j++) {
+        // int ht = Math.min(height.get(i), height.get(j));
+        // int width = j - i;
+        // int currWater = ht*width;
+        // maxWater = Math.max(maxWater,currWater);
+        // }
+        // }
+        // return maxWater;
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+        // 2 - Pointer approach -> O(n)
         int maxWater = 0;
-        for(int i = 0; i < height.size(); i++) {
-            for(int j = i + 1; j < height.size(); j++) {
-                int ht = Math.min(height.get(i), height.get(j));
-                int width = j - i;
-                int currWater = ht*width;
-                maxWater = Math.max(maxWater,currWater);
+        int lp = 0;
+        int rp = height.size() - 1;
+
+        while (lp < rp) {
+            // calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int wid = rp - lp;
+            int currWater = ht * wid;
+            maxWater = Math.max(maxWater, currWater);
+
+            // update ptr
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
             }
         }
         return maxWater;
