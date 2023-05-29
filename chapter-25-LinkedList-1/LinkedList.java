45864312891da629b132ca036cdb1a4c0ac69f34
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-30 00:23:54 +0530

    checkPalindrome

--- chapter-25-LinkedList-1/LinkedList.java
+++ chapter-25-LinkedList-1/LinkedList.java
@@ -192,12 +192,58 @@ public class LinkedList {
         return;
     }
 
+
+    //Palindrome
+    public Node  findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while ( fast != null && fast.next != null ) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+        }
+        return slow; //slow my middle 
+    }
+
+    public boolean checkPalindrome() {
+        if(head == null || head.next == null){
+            return true; 
+
+        }
+        //step1 - findmid
+        Node middle = findMid(head);
+
+        //step 2 - reverse 2nd half
+        Node prev = null;
+        Node curr = middle;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        Node right = prev; // right half heaqd
+        Node left = head;
+
+        //step3 - check left half and right half
+        while (right != null) {
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
     public static void main(String[] args) { 
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
         ll.addFirst(1);
-        ll.addLast(3);
-        ll.addLast(4);
+        ll.addLast(2);
+        ll.addLast(1);
 
         ll.printLL();
         // ll.addAtIndex(20, 2);
@@ -210,8 +256,9 @@ public class LinkedList {
 
         // ll.reverse();
 
-        ll.deleteNthfromEnd(2);
-        ll.printLL();
+        // ll.deleteNthfromEnd(2);
+        System.out.println(ll.checkPalindrome());
+        
 
     }
 }
