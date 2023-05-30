7cb9f448c317612ff62371697674f60dd62301ab
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-30 19:10:20 +0530

    addLast

--- chapter-25-LinkedList-1/DoubleLL.java
+++ chapter-25-LinkedList-1/DoubleLL.java
@@ -29,6 +29,19 @@ public class DoubleLL {
         head = newNode;
     }
 
+    //add last in doubly linked list
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
     //print dll
     public void printDll(){
         if(head == null){
@@ -49,6 +62,9 @@ public class DoubleLL {
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
+
+        dll.addLast(4);
+        dll.addLast(5);
 
         dll.printDll();
         
