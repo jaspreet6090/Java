c45048559e3aad37188b42665666dff8ac93a3b3
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-30 15:49:36 +0530

    Cycle detct

--- chapter-25-LinkedList-1/LinkedList2.java
+++ chapter-25-LinkedList-1/LinkedList2.java
@@ -13,36 +13,36 @@ public class LinkedList2 {
     public static Node tail;
     public static int size;
 
-    public void addFirst(int data) {
-        // step 1 - create a new node
-        Node newNode = new Node(data);
-        size++;
-        if (head == null) {
-            head = tail = newNode;
-            return;
-        }
-        // step2 = newNode next = head
-        newNode.next = head; // link
+    // public void addFirst(int data) {
+    //     // step 1 - create a new node
+    //     Node newNode = new Node(data);
+    //     size++;
+    //     if (head == null) {
+    //         head = tail = newNode;
+    //         return;
+    //     }
+    //     // step2 = newNode next = head
+    //     newNode.next = head; // link
 
-        // step3 = head = newNode
-        head = newNode;
-    }
+    //     // step3 = head = newNode
+    //     head = newNode;
+    // }
 
-    public void printLL() { // O(n)
-        if (head == null) {
-            System.out.println("LL is empty");
-            return;
-        }
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+    // public void printLL() { // O(n)
+    //     if (head == null) {
+    //         System.out.println("LL is empty");
+    //         return;
+    //     }
+    //     Node temp = head;
+    //     while (temp != null) {
+    //         System.out.print(temp.data + "->");
+    //         temp = temp.next;
+    //     }
+    //     System.out.println("null");
+    // }
 
     // check cycle or not
-    public boolean isCycle() {
+    public static boolean isCycle() { //Floyd's algorithm
         Node slow = head;
         Node fast = head;
 
@@ -59,6 +59,10 @@ public class LinkedList2 {
     public static void main(String[] args) {
         
         head = new Node(1);
-        
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        //1->2->3->1
+        System.out.println(isCycle());
     }
 }
