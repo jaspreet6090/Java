3d4caf17bef8cd3749a229dda4d2f5b519fc64a4
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-05-30 19:06:35 +0530

    addFirst & print in Doubly LL

---
+++ chapter-25-LinkedList-1/DoubleLL.java
@@ -0,0 +1,56 @@
+public class DoubleLL {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add first in doubly linked list
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //print dll
+    public void printDll(){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"<->");
+         temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.printDll();
+        
+    }
+}
