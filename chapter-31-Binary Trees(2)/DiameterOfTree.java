66b37d6f99ec2531da1daef7975ff3e5a0cd40c4
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-06-04 20:51:25 +0530

    DiameterOfTree Approach 1

---
+++ chapter-31-Binary Trees(2)/DiameterOfTree.java
@@ -0,0 +1,64 @@
+
+
+public class DiameterOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    //Approach 1 O(n^2)
+    public static int diameter(Node root) {
+        if(root == null) 
+            return 0;
+
+       int  leftDiam = diameter(root.left);
+       int  rightDiam = diameter(root.right);
+
+       int  lh = height(root.left);
+       int rh = height(root.right);
+
+       int selfDiam = lh + rh + 1;
+
+       return Math.max( selfDiam , Math.max(leftDiam, rightDiam));
+
+    }
+
+    //--------------------------------------------------------
+    //Approach 2 O(n)
+    
+
+    //Height of tree 
+    public static int height(Node root) {
+        if(root == null){
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+    public static void main(String[] args) {
+      /*         1
+               /   \
+              2     3
+             / \   /  \            
+            4   5 6   7
+        */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(diameter(root));
+    }
+}
