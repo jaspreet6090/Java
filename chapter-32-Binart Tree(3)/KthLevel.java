99fe47c598c4e6c441e24c2977f9709e3235dd58
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2023-06-05 21:40:39 +0530

    Approach 2

--- chapter-32-Binart Tree(3)/KthLevel.java
+++ chapter-32-Binart Tree(3)/KthLevel.java
@@ -17,47 +17,58 @@ public class KthLevel{
     //Approach 1 
 
 
-    // public static void level(Node root, int k){
-    //     //level order 
+    public static void level(Node root, int k){
+        //level order 
         
-    //     if(root == null){
-    //         return;
-    //     }
-    //     Queue<Node> q = new LinkedList<>();
-    //     q.add(root);
-    //     q.add(null);
-    //     int i =0;
-    //     while(!q.isEmpty()){
-    //         Node currNode = q.remove();
-    //         if(currNode == null){
-    //             i++;
-    //             // System.out.println();
-    //             if(q.isEmpty()){
-    //                 break;
-    //             } else {
-    //                 q.add(null);
-    //             }
-    //         } else {
-    //             if(i == k){
-    //                 System.out.print(currNode.data+" ");
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        int i =0;
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                i++;
+                // System.out.println();
+                if(q.isEmpty()){
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if(i == k){
+                    System.out.print(currNode.data+" ");
 
-    //             }
-    //              if(currNode.left != null){
-    //                 q.add(currNode.left);
+                }
+                 if(currNode.left != null){
+                    q.add(currNode.left);
 
-    //             }
-    //             if(currNode.right != null){
-    //                 q.add(currNode.right);
-    //             }
-    //         }
-    //     }
-    // }
+                }
+                if(currNode.right != null){
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
 
     //--------------------------------------------------------------------------------------------------------------------
 
     //Approach 2
 
-    public static void level(Node root , int level , int k) {
+    public static void KLevel(Node root , int level , int k) {
+         if(root == null){
+            return;
+
+         }
+         if(level == k ){
+            System.out.print(root.data+" ");
+            return;
+         }
+         KLevel(root.left, level+1 , k);
+         KLevel(root.right, level+1, k);
+
 
     }
     public static void main(String[] args) {
@@ -76,6 +87,8 @@ public class KthLevel{
         root.right.right = new Node(7);
 
 
-        level(root, 0,2);
+        level(root, 2);
+        System.out.println();
+        KLevel(root, 0, 2);
     }
 }
